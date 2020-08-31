8cb507317522aef91261290fb2f14a5d15503f04
Khue Doan <khuedoan98@gmail.com>
2020-08-31 20:31:39 +0700

    Use var block

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
     "fmt"
 )
 
-var name string = "Khue Doan"
-var age int = 22
+var (
+    name string = "Khue Doan"
+    age int = 22
+)
 
 func main() {
     fmt.Printf("Name: %v, Age: %v", name, age)
