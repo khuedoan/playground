73b5c2c663706631d153dd550a8f1ff956b4d185
Khue Doan <khuedoan98@gmail.com>
2020-09-06 12:09:20 +0700

    Try boolean

--- main.go
+++ main.go
@@ -2,15 +2,9 @@ package main
 
 import (
     "fmt"
-    "strconv"
-)
-
-var (
-    name string = "Khue Doan"
-    age int = 22
 )
 
 func main() {
-    var strAge string = strconv.Itoa(age)
-    fmt.Printf("Name: %v, Age: %v", name, strAge)
+    m := 1 == 1
+    fmt.Printf("%v", m)
 }
