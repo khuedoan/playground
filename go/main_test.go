6f15c93cb5155d8b51180c55348b3837aeee6423
Khue Doan <khuedoan98@gmail.com>
2021-01-22 14:13:13 +0700

    Yay TDD is working

--- go/main_test.go
+++ go/main_test.go
@@ -5,8 +5,8 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello world"
+	got := Hello("Khue Doan")
+	want := "Hello, Khue Doan"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
