abae1ed8b7a81218166a0ef38a136f820d7fe5e8
Khue Doan <khuedoan98@gmail.com>
2021-03-21 13:09:33 +0700

    Fix printer module

---
+++ go/printer/go.mod
@@ -0,0 +1,3 @@
+module github.com/khuedoan/playground/go/printer
+
+go 1.16
--- go/printer/printer.go
+++ go/printer/printer.go
@@ -1,5 +1,7 @@
 package printer
 
+import "fmt"
+
 func Hello() {
-	fmt.Println("unexported hello")
+	fmt.Println("Exported hello")
 }
