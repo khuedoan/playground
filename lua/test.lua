17a9caa6a20dfb5bf6284ad4ead0c0572efeb889
Khue Doan <khuedoan98@gmail.com>
2021-09-25 16:23:38 +0700

    Test lua table

---
+++ lua/test.lua
@@ -0,0 +1,10 @@
+print(table.concat({":LspInstall"
+    "dockerls",
+    "gopls",
+    "sumneko_lua",
+    "pyright",
+    "rust_analyzer",
+    "terraformls",
+    "tsserver",
+    "yamlls",
+}, " "))
