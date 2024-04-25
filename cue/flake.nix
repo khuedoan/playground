b3a85b12be2a97e55b47bd5eaa573fc30bc0ba05
Khue Doan <mail@khuedoan.com>
2024-04-25 10:09:17 +0700

    Cue

---
+++ cue/flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          packages = [
+            cue
+          ];
+        };
+      }
+    );
+}
