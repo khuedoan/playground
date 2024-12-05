32d288c01efa17e2da884fe49c5a5ff4dfead6fc
Khue Doan <mail@khuedoan.com>
2024-12-05 14:38:29 +0700

    New flake style

--- flake.nix
+++ flake.nix
@@ -4,28 +4,21 @@
   };
 
   outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
-    in
-    {
-      devShells = forAllSystems (system: {
-        default = pkgs.${system}.mkShellNoCC {
-          packages = with pkgs.${system}; [
-            # neovim
-            # ollama
-            # zed-editor
-            python312Packages.numpy
-            python312Packages.matplotlib
-            python312Packages.pytz
-          ];
-        };
-      });
-    };
+  let
+    supportedSystems = nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+  in
+  {
+    devShells = supportedSystems (system: {
+      default = with nixpkgs.legacyPackages.${system}; mkShell {
+        packages = [
+          neovim
+        ];
+      };
+    });
+  };
 }
