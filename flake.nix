48981feb0d129e233d19333e6fa5997faa8076cf
Khue Doan <mail@khuedoan.com>
2025-07-13 13:26:29 +0700

    feat: Elixir Livebook

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
   outputs = { self, nixpkgs }:
@@ -16,7 +16,7 @@
     devShells = supportedSystems (system: {
       default = with nixpkgs.legacyPackages.${system}; mkShell {
         packages = [
-          cue
+          livebook
         ];
       };
     });
